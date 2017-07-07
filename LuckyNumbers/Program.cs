e31fff1de06dced350229f805fd32c881c890543
Nick Wise <nickcwise@gmail.com>
2017-07-07 13:15:33 -0400

    Initial Commit

---
+++ LuckyNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PART 1
+            //Ask the user for a starting number for the lowest number in the number range.
+            //Ask the user for an ending number for the highest number in the number range.
+            //Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
+            //Numbers must be stored in an array
+            //Array must be populated using a loop
+            //If the user enters a number that is outside of the range set, prompt the user to give you a valid number. 
+            //Do this until the user enters a valid number.
+            //
+            //PART 2
+            //The program should randomly generate 6 numbers using a loop
+            //The randomly generated numbers should be stored in an array
+            //Numbers should be displayed to the console as such and using a loop (Numbers below are for example only. Format must be exact):
+            //Lucky Number: 12
+            //Lucky Number: 47
+            //Lucky Number: 28
+            //Lucky Number: 3
+            //Lucky Number: 19
+            //Lucky Number: 35
+            //
+            //PART 3
+            //Hard - code a value for the jackpot amount and let the user know what the jackpot amount is at some point you decide in the program.
+            //The program should count the number of correctly guessed numbers and output to the console to notify the user.Example:
+            //You guessed 3 numbers correctly!
+            //The program should calculate the user's winnings based on the number of numbers guessed correctly.
+
+            //The user's winnings should be output to the console. Example:
+            //You won $256, 877.23!
+            //
+            //PART 4
+            //Ask the user if the user would like to play again.
+            //If the user says yes, then the program should run again from the beginning.
+            //If the user says no, then the program should say "Thanks for playing!"(Must be exact statement).
+
+
+        }
+    }
+}
